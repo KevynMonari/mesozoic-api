36e18af9f214a19316450500795d03b29c2f1e9d
KevynMonari <Ksalesmonari@gmail.com>
2026-06-23 07:59:55 -0300

    chore: inicializacao do projeto

---
+++ src/main/java/com/prehistoria/mesozoic_api/MesozoicApiApplication.java
@@ -0,0 +1,13 @@
+package com.prehistoria.mesozoic_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MesozoicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MesozoicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prehistoria/mesozoic_api/MesozoicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prehistoria.mesozoic_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MesozoicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
