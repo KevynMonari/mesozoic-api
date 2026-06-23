e760f916f13b212e431fdb6788aa2c582322a991
KevynMonari <Ksalesmonari@gmail.com>
2026-06-23 16:16:07 -0300

    feat: CRUD completo de regioes e fossies

--- src/main/java/com/prehistoria/mesozoic_api/controllers/FossilControler.java
+++ src/main/java/com/prehistoria/mesozoic_api/controllers/FossilControler.java
@@ -3,6 +3,7 @@ package com.prehistoria.mesozoic_api.controllers;
 import com.prehistoria.mesozoic_api.entities.Fossil;
 import com.prehistoria.mesozoic_api.repositories.FossilRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,5 +23,37 @@ public class FossilControler {
     @PostMapping
     public Fossil salvar(@RequestBody Fossil fossil){
         return fossilRepository.save(fossil);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Fossil> atualizar (@PathVariable Long id, @RequestBody Fossil dadosAtualizados){
+        return fossilRepository.findById(id)
+                .map(fossilExistente -> {
+                    fossilExistente.setTipoFossil(dadosAtualizados.getTipoFossil());
+                    fossilExistente.setAnoDescoberta(dadosAtualizados.getAnoDescoberta());
+                    fossilExistente.setPaleontologo(dadosAtualizados.getPaleontologo());
+
+                    if (dadosAtualizados.getCriatura() != null){
+                        fossilExistente.setCriatura(dadosAtualizados.getCriatura());
+                    }
+
+                    if (dadosAtualizados.getRegiao() != null){
+                        fossilExistente.setRegiao(dadosAtualizados.getRegiao());
+                    }
+
+                    Fossil fossilSalvo = fossilRepository.save(fossilExistente);
+                    return ResponseEntity.ok(fossilSalvo);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        return fossilRepository.findById(id)
+                .map(fossil -> {
+                    fossilRepository.delete(fossil);;
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/prehistoria/mesozoic_api/controllers/RegiaoController.java
+++ src/main/java/com/prehistoria/mesozoic_api/controllers/RegiaoController.java
@@ -3,9 +3,8 @@ package com.prehistoria.mesozoic_api.controllers;
 import com.prehistoria.mesozoic_api.entities.Regiao;
 import com.prehistoria.mesozoic_api.repositories.RegiaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,35 @@ public class RegiaoController {
     @GetMapping
     public List<Regiao> listarTodas(){
         return regiaoRepository.findAll();
+    }
+
+    @PostMapping
+    public Regiao salvar(@RequestBody Regiao regiao){
+        return regiaoRepository.save(regiao);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Regiao> atualizar(@PathVariable Long id, @RequestBody Regiao dadosAtualizados){
+        return regiaoRepository.findById(id)
+                .map(regiaoExistente -> {
+                    regiaoExistente.setNomeLocal(dadosAtualizados.getNomeLocal());
+                    regiaoExistente.setEstado(dadosAtualizados.getEstado());
+                    regiaoExistente.setPais(dadosAtualizados.getPais());
+
+                    Regiao regiaoSalva = regiaoRepository.save(regiaoExistente);
+                    return ResponseEntity.ok(regiaoSalva);
+
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        return regiaoRepository.findById(id)
+                .map(regiao -> {
+                    regiaoRepository.delete(regiao);
+                    return ResponseEntity.noContent().<Void>build();
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
